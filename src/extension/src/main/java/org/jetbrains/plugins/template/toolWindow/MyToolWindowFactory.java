d6b261fe9735812ac101de5d216e919bf22d9b28
Diana <dianavulpoi3@gmail.com>
2025-04-05 21:42:27 +0300

    demo scrolling pane

--- src/extension/src/main/java/org/jetbrains/plugins/template/toolWindow/MyToolWindowFactory.java
+++ src/extension/src/main/java/org/jetbrains/plugins/template/toolWindow/MyToolWindowFactory.java
@@ -1,39 +1,27 @@
 package org.jetbrains.plugins.template.toolWindow;
 
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.components.JBLabel;
-import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import org.jetbrains.plugins.template.MyBundle;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.template.services.MyProjectService;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 
-public class MyToolWindowFactory implements ToolWindowFactory {
-
-    private static final Logger LOG = Logger.getInstance(MyToolWindowFactory.class);
-
-    public MyToolWindowFactory() {
-        LOG.warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.");
-    }
+public class MyToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
 
     @Override
-    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
         Content content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false);
         toolWindow.getContentManager().addContent(content);
     }
 
     @Override
-    public boolean shouldBeAvailable(Project project) {
+    public boolean shouldBeAvailable(@NotNull Project project) {
         return true;
     }
 
@@ -42,25 +30,40 @@ public class MyToolWindowFactory implements ToolWindowFactory {
         private final MyProjectService service;
 
         public MyToolWindow(ToolWindow toolWindow) {
+            // If you're using the built-in IntelliJ service, you can grab it here:
             this.service = ServiceManager.getService(toolWindow.getProject(), MyProjectService.class);
         }
 
         public JPanel getContent() {
-            JBPanel<?> panel = new JBPanel<>();
+            // Create a main panel (using BorderLayout as a convenient container layout)
+            JPanel mainPanel = new JPanel(new BorderLayout());
 
-            JBLabel label = new JBLabel(MyBundle.message("randomLabel", "?"));
-            panel.add(label);
+            // Create a panel to hold the pictograms in a vertical list
+            JPanel pictogramPanel = new JPanel();
+            pictogramPanel.setLayout(new BoxLayout(pictogramPanel, BoxLayout.Y_AXIS));
 
-            JButton button = new JButton(MyBundle.message("shuffle"));
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    label.setText(MyBundle.message("randomLabel", service.getRandomNumber()));
-                }
-            });
+            // Example: Add a few pictograms (labels with icons) for demonstration
+            // In a real plugin, you might retrieve these from your MyProjectService or elsewhere.
+            for (int i = 0; i < 50; i++) {
+                // Replace with your actual icon:
+                //  e.g. Icon icon = AllIcons.General.InspectionsOK; or a custom icon
+                Icon dummyIcon = UIManager.getIcon("OptionPane.informationIcon");
+                JLabel label = new JLabel("Workspace " + i, dummyIcon, SwingConstants.LEFT);
 
-            panel.add(button);
-            return panel;
+                // Add some spacing or set a preferred size if needed:
+                label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+                pictogramPanel.add(label);
+            }
+
+            // Wrap the pictogram panel in a scroll pane
+            JScrollPane scrollPane = new JScrollPane(pictogramPanel,
+                    JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                    JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            // Add the scroll pane to the main panel
+            mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+            return mainPanel;
         }
     }
 }
